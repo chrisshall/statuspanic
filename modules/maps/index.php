4e32f992e800b4140c811a7b010249a2765487e1
chrisshall <33465789+chrisshall@users.noreply.github.com>
2017-11-16 09:44:15 -0500

    Added Google Map
    Google Map added. Need to design board to show travel times

--- modules/maps/index.php
+++ modules/maps/index.php
@@ -1,10 +1,21 @@
 <?php
 
+$travel = file_get_contents('https://maps.googleapis.com/maps/api/distancematrix/json?origins=860+Spring+St+NW,+Atlanta,+GA&destinations=Duluth,+GA|Mall+of+GA+Buford,+GA|Athens,+GA|&departure_time=now&traffic_model=best_guess&key=AIzaSyD0WVEFw7a4V7zgEAdHkr5YbDu3fTw4RW8');
+$data = json_decode($travel, true);
 
-/*
-<html>
-	<img src="modules/maps/martaredline.png" width= "400px">
-	<img src="modules/maps/martagoldline.gif" width="400px">
-</html
-*/
+$time_duluth= $data['rows'][0]['elements'][0]['duration_in_traffic']["text"];
+$time_buford= $data['rows'][0]['elements'][1]['duration_in_traffic']["text"];
+$time_athens= $data['rows'][0]['elements'][2]['duration_in_traffic']["text"];
+
 ?>
+<html>
+<iframe
+  width="600"
+  height="450"
+  frameborder="0" style="border:0"
+  src="https://www.google.com/maps/embed/v1/search?key=AIzaSyD0WVEFw7a4V7zgEAdHkr5YbDu3fTw4RW8
+    &q=NCR,Atlanta+GA" allowfullscreen>
+</iframe>
+
+
+</html>
