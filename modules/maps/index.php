e89cf483627031438f8612398b3b8e9ba6660640
chrisshall <33465789+chrisshall@users.noreply.github.com>
2017-11-20 12:54:43 -0500

    Fixed Google Map
    now focuses on headquarters

--- modules/maps/index.php
+++ modules/maps/index.php
@@ -6,7 +6,7 @@
   height="450"
   frameborder="0" style="border:0"
   src="https://www.google.com/maps/embed/v1/search?key=AIzaSyBQ_PLwliFdOCBWiIed_s4IBrZc1q7DZYo
-    &q=NCR,Atlanta+GA" allowfullscreen>
+    &q=NCR+Headquarters+860+Spring+St+NW,Atlanta+GA" allowfullscreen>
 </iframe>
 
 
