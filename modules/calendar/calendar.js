80a17e6379ecd82dd88d36a1872da95a4ecfd7d1
chrisshall <33465789+chrisshall@users.noreply.github.com>
2017-12-07 08:51:22 -0500

    Calendar Buttons
    They Work!!

--- modules/calendar/calendar.js
+++ modules/calendar/calendar.js
@@ -1,3 +1,5 @@
+//var month_year_string = document.getElementById('month').innerHTML;
+
 function goBack(month_year){
 	var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 	var date_array = month_year.split(" ");
@@ -13,9 +15,22 @@ function goBack(month_year){
 		var date = months[index-1] + " " + year;
 	}
 	//console.log(index);
+	//setMonthYear(date);
 	document.getElementById('month').innerHTML = date;
-	
+	//console.log(date);
+	$.post("modules/calendar/index.php",{month_year: date},function(data){
+			$('#result').html(data);
+	});
 }
+function setMonthYear(month_year){
+	month_year_string = month_year;
+}
+
+function getMonthYear(){
+	return month_year_string;
+}
+	
+
 function goAhead(month_year){
 	var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 	var date_array = month_year.split(" ");
@@ -31,6 +46,46 @@ function goAhead(month_year){
 	else{
 		var date = months[index+1] + " " + year;
 	}
+	//setMonthYear(date);
 	document.getElementById('month').innerHTML = date;
+	$.post("modules/calendar/index.php",{month_year: date},function(data){
+			$('#result').html(data);
+			console.log(data);
+	});
 }	
+/*
+$('#nextmonth').click(function(){
+	$.ajax({
+		type: 'POST',
+		url: '/modules/calendar/index.php',
+		data: {'month_year': month_year_string},
+		datatype: 'text',
+		success: function(data){
+			//console.log(month_year_string);
+			$('#result').html(data);
+		}
+	});
+});
+$('#lastmonth').click(function(){
+	$.ajax({
+		type: 'POST',
+		url: '/modules/calendar/index.php',
+		data: {'month_year': month_year_string},
+		datatype: 'text',
+		success: function(data){
+			$('#result').html(data);
+		}
+		
+	});
+});*/
 
+/*if (window.XMLHttpRequest) {
+  xmlhttp = new XMLHttpRequest();
+  xmlhttp.onreadystatechange = function () {
+    if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
+      alert('form was submitted');
+    }
+  };
+  xmlhttp.open("POST", "modules/calendar/index.php", true);
+  xmlhttp.send("month_year="+month_year_string);
+}*/
--- modules/calendar/index.php
+++ modules/calendar/index.php
@@ -8,56 +8,76 @@ date_default_timezone_set('America/New_York');
 $date = new DateTime();
 
 
-//Get month and year of what's in the calendar title
-
-
-// return today's date as an int (i.e 24)
-$today = $date -> format('d');
-
-//subtract 23 days
-$first = date('U',time()-60*60*24*($today-1));
-//echo $first;
-$day = getdate($first);
-//print_r($day);
-
-//Return the weekday of the first day of the month, so we know where to put start the 2D array
-$first_day = $day['wday']; // 0 for Sunday, 6 for Saturday
-
-//Now that we have the weekday, we can add the entire week's numbers by subtracting for going back into last month's or adding +1 for next elements. words are hard.
-$calendar[0][$first_day]='1';
-for ($i=1; $i<=$first_day; $i++){
-	$date = getdate($day[0]-(60*60*24*$i));
-	$calendar[0][$first_day-$i]=$date['mday'];
+//Get month and year of what's in the calendar title via ajax
+if (isset($_POST['month_year']) && !empty($_POST['month_year'])){
+		$month_year = $_POST['month_year'];
+	$month_year = explode(' ',$month_year);
+	$month = $month_year[0];
+	$year = $month_year[1];
+	//echo "<script type='text/javascript'> console.log('".$month_year[0]."');</script>";
+	//print_r($month_year);
+	//echo "<script type='text/javascript'> console.log('$month_year');</script>";
+	
+	//first day of month needs to be converted to int
+	$first_day_string = $month . ' 1 ' . $year;
+	$first_day_int = date('U', strtotime($first_day_string));
+	//echo "<script type='text/javascript'>alert('$first_day_int');</script>";
+	$first = $first_day_int;
+	
 }
-// Next, we'll get the rest of this week's
-for ($i=3; $i<=6; $i++){
-	$date = getdate($day[0]+(60*60*24)*($i-3));
-	$calendar[0][$i] = $date['mday'];
-}
+ else{
+	$month = date('F');
+	$year = date('Y');
+	// return today's date as an int (i.e 24)
+	$today = $date -> format('d');
 
-//Now set $day to int value of first day of next week
-$day = getdate(date('U', $date[0]+60*60*24));
-//echo (getdate($day)) ['mday'];
+	//subtract 23 days
+	$first = date('U',time()-60*60*24*($today-1));
+	//echo $first
+ }
+	$day = getdate($first);
+	//print_r($day);
 
-//print_r($day);
-for ($col=0;$col<=6;$col++){	
-	for ($row=1; $row <= 4; $row++){
-		$date=getdate($day[0]+(60*60*24)*(7*($row-1)+$col));
-		$calendar[$row][$col] = $date['mday'];
-		//echo $date['mday'] . ", ";
+	//Return the weekday of the first day of the month, so we know where to put start the 2D array
+	$first_day = $day['wday']; // 0 for Sunday, 6 for Saturday
+
+	//Now that we have the weekday, we can add the entire week's numbers by subtracting for going back into last month's or adding +1 for next elements. words are hard.
+	$calendar[0][$first_day]='1';
+	for ($i=1; $i<=$first_day; $i++){
+		$date = getdate($day[0]-(60*60*24*$i));
+		$calendar[0][$first_day-$i]=$date['mday'];
 	}
-	//echo "<br>";
-}
+	// Next, we'll get the rest of this week's
+	for ($i=$first_day; $i<=6; $i++){
+		$date = getdate($day[0]+(60*60*24)*($i-$first_day));
+		$calendar[0][$i] = $date['mday'];
+	}
+
+	//Now set $day to int value of first day of next week
+	$day = getdate(date('U', $date[0]+60*60*24));
+	//echo (getdate($day)) ['mday'];
+
+	//print_r($day);
+	for ($col=0;$col<=6;$col++){	
+		for ($row=1; $row <= 4; $row++){
+			$date=getdate($day[0]+(60*60*24)*(7*($row-1)+$col));
+			$calendar[$row][$col] = $date['mday'];
+			//echo $date['mday'] . ", ";
+		}
+		//echo "<br>";
+	}
+ 
 ?>
 
 
 
 <html>
-<div class='calendar' >
-	<script src="modules/calendar/calendar.js"></script>	
+<div id="result">
+<div class='calendar'>
+	<script src="modules/calendar/calendar.js","resources/jquery.js"></script>	
 		<button class="button" id="lastmonth" style="float: left" onclick="goBack(document.getElementById('month').innerHTML)"> < </button>
 		<button class="button" onclick="goAhead(document.getElementById('month').innerHTML)" id="nextmonth" style="float: right"> > </button>
-		<div id="month" style="font-size: 30px"><?php echo date('F Y')?></div>	
+		<div id="month" style="font-size: 30px"><?php echo $month . " " . $year?></div>	
 		
 </div>
 
@@ -94,7 +114,7 @@ for ($col=0;$col<=6;$col++){
 							" . $calendar[$row][$col] . "</td>";		
 					}
 					else{
-						if ($calendar[$row][$col] == getdate(time())['mday']){
+						if (($calendar[$row][$col] == getdate(time())['mday'])&& $month == date('F') && $year == date('Y')){
 							echo "<td class='td'>
 								<button class='today'>
 								" . $calendar[$row][$col] . "</button></td>";
@@ -110,4 +130,5 @@ for ($col=0;$col<=6;$col++){
 			?>
 				
 	</table>
+	</div>
 </html>
