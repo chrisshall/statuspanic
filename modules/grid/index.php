df85de32868ddfc26692fa0e13b1f01cb68a2e4c
chrisshall <33465789+chrisshall@users.noreply.github.com>
2018-02-01 13:29:10 -0500

    SCER Grid expansion
    Click on a scer to expand it to view details

--- modules/grid/index.php
+++ modules/grid/index.php
@@ -26,16 +26,91 @@ $data = json_decode($str, true);
 		<th class='grid th'>Customer</th>
 		<th class='grid th'>Need Date</th>
     <?php
-    foreach($data as $row) {
-		echo "<tr class='tr'>";
+    for($i = 0; $i < count($data) -1; $i++) {
+		//print_r(array_keys($row));
+		$row = $data[$i];
+		echo "<tr class='tr' id='tr_row_" . $i ."'>";
             echo "<td class='td'> " . 'ATL' . $row['scer #'] . "</td>";
 			echo "<td class='td'> " . $row['assigned_to'] . "</td>";
 			echo "<td class='td'> " . $row['Customer'] . "</td>";
 			echo "<td class='td'> " . $row['need_date'] . "</td>";
-        echo '</tr>';
-    }
-    
+        echo '</tr>';	
+		echo "<tr style='visibility:hidden;height:0px;display:table-column;' id='tr_" . $i . "'> <td></td><td></td><td></td><td> <table style='visibility:hidden; height=0;' id='scer_" . $i . "'>";
+
+		foreach(array_keys($row) as $keys){
+		 
+			echo "<tr class='tr'>";
+				echo "<td class = 'alt td'> " . $keys . "</td>";		// display key first
+				echo "<td class = 'alt td'> " . $row[$keys] . "</td>";	//now display value
+			echo '</tr>';
+		
+		}
+		echo '</table></td></tr>';
+	}
+	echo '</table>';
     ?>
-    
-    </table>
+	
 </div>
+
+<script>
+<?php 
+	
+	for ($i = 0; $i< count($data); $i++){
+?>	
+		var open_<?php echo $i ?> = false;
+<?php
+				}
+?>
+	
+	function checkOthersOn(target){
+<?php 
+		
+		for ($i = 0; $i< count($data); $i++){
+?>	
+			if (open_<?php echo $i ?> == true){
+					open_<?php echo $i ?> = false;
+					document.getElementById("scer_<?php echo $i ?>").style.visibility= "hidden";
+					document.getElementById("tr_<?php echo $i ?>").style.display= "table-column";
+					document.getElementById("tr_<?php echo $i ?>").style.visibility= "hidden";
+			}
+			
+			
+<?php
+		}
+?>
+	}
+		
+<?php  
+	for ($i = 0; $i<count($data) -1; $i++){
+?>
+	//console.log(document.getElementById('tr_row<?php echo $i?>'));
+		document.getElementById('tr_row_<?php echo $i?>').addEventListener('click', 
+		function(event) {
+			if (!open_<?php echo $i?>){
+				checkOthersOn(event.target);
+				console.log(event.target);
+				document.getElementById(("scer_<?php echo $i ?>")).style.visibility = "visible";
+				document.getElementById("tr_<?php echo $i ?>").style.visibility = "visible";
+				document.getElementById("tr_<?php echo $i?>").style.display = "";
+			}
+			
+			else{
+					document.getElementById("scer_<?php echo $i ?>").style.visibility= "hidden";
+					document.getElementById("tr_<?php echo $i ?>").style.display= "table-column";
+					document.getElementById("tr_<?php echo $i ?>").style.visibility= "hidden";
+			}
+			
+			open_<?php echo $i?> = !open_<?php echo $i?>;
+			
+		});	
+<?php
+	}
+?>	
+
+
+	
+	
+</script>
+
+
+
